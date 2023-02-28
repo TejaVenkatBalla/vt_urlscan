69675f472de4ad3b85f4051e47ffda69c3993a44
TejaVenkatBalla <tejavenkatballa@gmail.com>
2023-02-28 18:26:43 +0530

    name change

--- src/components/vt2.js
+++ src/components/vt2.js
@@ -1,34 +1,23 @@
-const API_KEY = 'ee9a95de89da158a7983bfe95d3f54d5027a0f951d4b16ab0438bb04e25285f6';
-
 const VirusTotalScanner2 = () => {
   const [url, setUrl] = useState('');
   const [result, setResult] = useState(null);
   const [loading, setLoading] = useState(false);
-  const [analysisId, setAnalysisId] = useState(null);
 
   const handleSubmit = (event) => {
     event.preventDefault();
     setLoading(true);
     setResult(null);
 
-    const options = {
+    const requestOptions = {
       method: 'POST',
-      headers: {
-        'x-apikey': API_KEY,
-        'accept': 'application/json',
-        'content-type': 'application/x-www-form-urlencoded',
-      },
-      body: JSON.stringify({
-        url: url
-      }),
-      mode: 'cors',
-      credentials: 'omit',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ url: url })
     };
 
-    fetch(`https://www.virustotal.com/api/v3/urls?url=${encodeURIComponent(url)}`, options)
+    fetch('/api/virustotal', requestOptions)
       .then(response => response.json())
       .then(data => {
-        setAnalysisId(data.data.id);
+        setResult(data);
         setLoading(false);
       })
       .catch(error => {
@@ -37,39 +26,6 @@ const VirusTotalScanner2 = () => {
         setLoading(false);
       });
   }
-
-  useEffect(() => {
-    const checkScanStatus = () => {
-      if (!analysisId) {
-        return;
-      }
-
-      const options = {
-        method: 'GET',
-        headers: {
-          'x-apikey': API_KEY,
-          accept: 'application/json',
-        },
-        mode: 'cors'
-      };
-
-      fetch(`https://www.virustotal.com/api/v3/analyses/${analysisId}`, options)
-        .then(response => response.json())
-        .then(data => {
-          if (data.data.attributes.status === 'completed') {
-            setResult(data);
-            setAnalysisId(null);
-          }
-        })
-        .catch(error => {
-          console.error(error);
-        });
-    }
-
-    const intervalId = setInterval(checkScanStatus, 5000);
-
-    return () => clearInterval(intervalId);
-  }, [analysisId]);
 
   return (
     <div>
@@ -80,20 +36,13 @@ const VirusTotalScanner2 = () => {
           <input type="text" value={url} onChange={event => setUrl(event.target.value)} />
         </label>
         <button type="submit" disabled={loading}>Scan</button>
-        <p>The scan will take few seconds</p>
+        <p>The scan will take a few seconds</p>
       </form>
       {loading && <p>Scanning...</p>}
       {result && (
         <div>
           <p>Scan results:</p>
           <pre>{JSON.stringify(result, null, 2)}</pre>
-          {result.data.attributes.malicious ?
-            <p>The URL is malicious</p>
-            :
-            <div>
-              <p>The URL is safe</p>
-            </div>
-          }
         </div>
       )}
     </div>
